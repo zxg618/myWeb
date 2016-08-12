e5cee619860ddd656e76673d016e7e2d322c3e03
zxg618 <zxg618@gmail.com>
2016-08-12 11:16:22 +1000

    main page

--- View/scripts/app.js
+++ View/scripts/app.js
@@ -2,6 +2,16 @@
 
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', function() {
-    //
+myApp.controller('mainController', function($scope) {
+    //global variable $scope?
+    //$scope.name = "Shawn";
+    //$scope.page = "index";
+    //$scope.getName = function() {
+    //    return "Shawn's homepage";
+    //};
+    //console.log($scope);
+
+    $scope.validate = function() {
+        console.log($scope.name);
+    };
 });
